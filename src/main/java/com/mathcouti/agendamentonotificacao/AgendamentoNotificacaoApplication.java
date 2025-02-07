2f07352493d238556157df1795a723ee4d0e7fdf
Matheus <mathcoutipro@gmail.com>
2025-02-07 08:45:03 -0300

    commit inicial

---
+++ src/main/java/com/mathcouti/agendamentonotificacao/AgendamentoNotificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.mathcouti.agendamentonotificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendamentoNotificacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgendamentoNotificacaoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mathcouti/agendamentonotificacao/AgendamentoNotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathcouti.agendamentonotificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendamentoNotificacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
